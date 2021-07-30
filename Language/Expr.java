9608637ba582bf6e22e4219d9ec51dff441d0f88
beautifulleaf <34896530+beautifulleaf@users.noreply.github.com>
2021-07-29 21:02:21 -0400

    Update Expr.java

--- Language/Expr.java
+++ Language/Expr.java
@@ -4,66 +4,68 @@ import java.util.List;
 
 abstract class Expr {
     interface Visitor<R> {
-    R visitBinaryExpr(Binary expr);
-    R visitGroupingExpr(Grouping expr);
-    R visitLiteralExpr(Literal expr);
-    R visitUnaryExpr(Unary expr);
-    R visitTernaryExpr(Ternary expr);
-  }
+        R visitBinaryExpr(Binary expr);
+        R visitGroupingExpr(Grouping expr);
+        R visitLiteralExpr(Literal expr);
+        R visitUnaryExpr(Unary expr);
+        R visitTernaryExpr(Ternary expr);
+        R visitVariableExpr(Variable expr);
+        R visitAssignExpr(Assign expr);
+    }
     static class Binary extends Expr {
-    Binary(Expr left, Token operator, Expr right) {
-     this.left = left;
-     this.operator = operator;
-     this.right = right;
-    }
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitBinaryExpr(this);
-    }
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitBinaryExpr(this);
+        }
 
-     final Expr left;
-     final Token operator;
-     final Expr right;
-  }
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
     static class Grouping extends Expr {
-    Grouping(Expr expression) {
-     this.expression = expression;
-    }
+        Grouping(Expr expression) {
+            this.expression = expression;
+        }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitGroupingExpr(this);
-    }
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitGroupingExpr(this);
+        }
 
-     final Expr expression;
-  }
+        final Expr expression;
+    }
     static class Literal extends Expr {
-    Literal(Object value) {
-     this.value = value;
-    }
+        Literal(Object value) {
+            this.value = value;
+        }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitLiteralExpr(this);
-    }
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitLiteralExpr(this);
+        }
 
-     final Object value;
-  }
+        final Object value;
+    }
     static class Unary extends Expr {
-    Unary(Token operator, Expr right) {
-     this.operator = operator;
-     this.right = right;
-    }
+        Unary(Token operator, Expr right) {
+            this.operator = operator;
+            this.right = right;
+        }
 
-    @Override
-    <R> R accept(Visitor<R> visitor) {
-    return visitor.visitUnaryExpr(this);
-    }
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitUnaryExpr(this);
+        }
 
-     final Token operator;
-     final Expr right;
-  }
+        final Token operator;
+        final Expr right;
+    }
     static class Ternary extends Expr {
         Ternary(Expr condition, Expr result, Expr altResult) {
             this.condition = condition;
@@ -78,5 +80,34 @@ abstract class Expr {
         final Expr result;
         final Expr altResult;
     }
-   abstract <R> R accept(Visitor <R> visitor);
+
+    static class Variable extends Expr {
+        Variable(Token name) {
+            this.name = name;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitVariableExpr(this);
+        }
+
+        final Token name;
+    }
+
+    static class Assign extends Expr {
+        Assign(Token name, Expr value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitAssignExpr(this);
+        }
+
+        final Token name;
+        final Expr value;
+    }
+
+    abstract <R> R accept(Visitor <R> visitor);
 }
