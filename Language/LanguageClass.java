240a0423f1bd86287253640cc577631b22647747
beautifulleaf <34896530+beautifulleaf@users.noreply.github.com>
2021-08-22 22:17:33 -0400

    Update LanguageClass.java

--- Language/LanguageClass.java
+++ Language/LanguageClass.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 public class LanguageClass implements LanguageCallable {
     final String name;
+    final LanguageClass superclass;
     private final Map<String, LanguageFunction> methods;
 
-    LanguageClass(String name, Map<String, LanguageFunction> methods) {
+    LanguageClass(String name, LanguageClass superclass, Map<String, LanguageFunction> methods) {
+        this.superclass = superclass;
         this.name = name;
         this.methods = methods;
     }
@@ -15,6 +17,10 @@ public class LanguageClass implements LanguageCallable {
     LanguageFunction findMethod(String name) {
         if (methods.containsKey(name)) {
             return methods.get(name);
+        }
+
+        if (superclass != null) {
+            return superclass.findMethod(name);
         }
 
         return null;
